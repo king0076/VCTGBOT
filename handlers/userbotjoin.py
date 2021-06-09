a16ea4fd6bf9a54a9dd6dfa65ed7e05bd5687794
king0076 <85625304+king0076@users.noreply.github.com>
2021-06-09 18:33:19 +0530

    Update userbotjoin.py

--- handlers/userbotjoin.py
+++ handlers/userbotjoin.py
@@ -27,18 +27,18 @@ async def addchannel(client, message):
         await USER.send_message(message.chat.id,"I joined here as you requested")
     except UserAlreadyParticipant:
         await message.reply_text(
-            "<b>@TheMp3play already in your chat</b>",
+            "<b>@Cobramusicyt already in your chat</b>",
         )
         pass
     except Exception as e:
         print(e)
         await message.reply_text(
             f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your group due to heavy join requests for userbot! Make sure user is not banned in group."
-            "\n\nOr manually add @TheMp3play to your Group and try again</b>",
+            "\n\nOr manually add @Cobramusicyt to your Group and try again</b>",
         )
         return
     await message.reply_text(
-            "<b>@TheMp3play userbot joined your chat</b>",
+            "<b>@Cobramusicyt userbot joined your chat</b>",
         )
     
 @USER.on_message(filters.group & filters.command(["userbotleave"]))
